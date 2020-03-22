e6873dce7da214e11522614bce882854ce964092
Yahyajaz <60370152+Yahyajaz@users.noreply.github.com>
2020-03-22 13:19:40 -0400

    Population Correlation Coefficient

---
+++ population_correlation_coefficient.py
@@ -0,0 +1,15 @@
+from FileReader import readCSV
+import numpy
+
+dataSet = readCSV('test.csv')
+
+def population_corelation_coefficient(dataSet):
+    if len(dataSet) == 0:
+        return 0
+    
+    cal_mean = sum(dataSet) / len(dataSet)
+    return (numpy.cov(dataSet)/cal_mean)
+
+
+#dataSet = [1,2,3,4,5,6,7,8,9,10]
+print (population_corelation_coefficient(dataSet))
