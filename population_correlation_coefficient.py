cb42ab93854373686297155090e851873f0eac24
Yahyajaz <60370152+Yahyajaz@users.noreply.github.com>
2020-03-23 10:48:44 -0400

    Update population_correlation_coefficient.py

--- population_correlation_coefficient.py
+++ population_correlation_coefficient.py
@@ -1,15 +1,20 @@
+import math
 from FileReader import readCSV
-import numpy
 
-dataSet = readCSV('data.csv')
+x = readCSV("xvalues.csv")
+y= readCSV("yvalues.csv")
 
-def population_corelation_coefficient(dataSet):
-    if len(dataSet) == 0:
+def population_corelation_coefficient(x,y):
+    if len(x) == 0 or len(y) == 0 or len(x) != len(y):
         return 0
-    
-    cal_mean = sum(dataSet) / len(dataSet)
-    return (numpy.cov(dataSet)/cal_mean)
+    s = 0
+    x_mean = sum(x)/len(x)
+    y_mean = sum(y)/len(y)
+    x_SD = math.sqrt(sum([(val - x_mean)**2 for val in x])/(len(x) - 1))
+    y_SD = math.sqrt(sum([(val - y_mean)**2 for val in y])/(len(y) - 1))
+    for i, j in zip(x,y):
+        s = s + (((i-x_mean)/x_SD)*((j-y_mean)/y_SD))
+    return s/len(x)
 
 
-#dataSet = [1,2,3,4,5,6,7,8,9,10]
-print (population_corelation_coefficient(dataSet))
+print (population_corelation_coefficient(x,y))
