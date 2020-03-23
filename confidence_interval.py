4565e8d170ae96c829968692c0f75cc5af0f8b5b
Yahyajaz <60370152+Yahyajaz@users.noreply.github.com>
2020-03-23 08:58:57 -0400

    Update confidence_interval.py

--- confidence_interval.py
+++ confidence_interval.py
@@ -5,15 +5,15 @@ confidence = 0.95
 dataSet = readCSV('test.csv')
 
 def confidenceInterval(dataSet):
-    n = len(dataSet)
-    m = sum(dataSet) / n
-    std = math.sqrt(sum([(val - m)**2 for val in dataSet])/(len(dataSet) - 1))
-    std_err = std / math.sqrt(n)
-    t = m / std_err
-    h = std_err * t * float(1 + confidence) / float(2., n - 1)
+    a = len(dataSet)
+    b = sum(dataSet) / a
+    std = math.sqrt(sum([(val - b)**2 for val in dataSet])/(len(dataSet) - 1))
+    std_err = std / math.sqrt(a)
+    t = b / std_err
+    h = std_err * t * float(1 + confidence) / float(2., a - 1)
 
-    start = m + h
-    end = m - h
+    start = b + h
+    end = b - h
     
     return start, end
 
